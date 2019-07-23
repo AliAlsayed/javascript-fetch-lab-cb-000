4cf78a266bccc008009fb73c43aa2fcca36a1f71
Ali Alsayed <a_alsayed87@hotmail.com>
2019-07-23 05:36:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ function showResults(json) {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'learn-co-curriculum/javascript-fetch-lab';
+  const token = '316326c0b0b618b83458c172ecd88edae2f5050c';
   //use fetch to fork it!
-  fetch(`https://api.github.com/repos/${owner}`)
+  fetch(`https://api.github.com/repos/${owner}`, {
+    method: 'POST',
+    headers: {
+      Authorization: 
+    }
+  })
 }
 
 function getToken() {
